c8529e91a6ebb057bea8c5b3a8d5e9977e4972be
Marina18 <marina@ersilia.io>
2026-09-01 12:16:38 +0200

    update columns

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -29,6 +29,6 @@ for smi in tqdm(smiles_list, desc="Sampling"):
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow([f"smiles_{i:02d}" for i in range(100)])  # header
+    writer.writerow([f"smi_{i:02d}" for i in range(100)])  # header
     for o in outputs:
         writer.writerow(o)
